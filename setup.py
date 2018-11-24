70da5144ddbb18905e55531acb17b922c549d6e9
truedread <truedread11@gmail.com>
2018-11-23 21:33:40 -0500

    Add .travis.yml and update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
+"""Setup module"""
+
 from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    LONG_DESCRIPTION = fh.read()
 
 setup(
     name='pymsl',
-    version='1.1',
+    version='1.2',
     description='Python library for interacting with the Netflix MSL API',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     url='https://github.com/truedread/pymsl',
     author='truedread',
     author_email='truedread11@gmail.com',
@@ -12,12 +19,13 @@ setup(
     install_requires=['pycryptodomex', 'requests'],
     classifiers=[
         'Environment :: Console',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Utilities'
     ]
 )
